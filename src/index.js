ad742472e9de8b9c289b1110e20ca964a6ab3886
mastyaeva.a <mastyaeva.a@it-universe.ru>
2020-11-12 20:34:58 +0400

    highlight winners line

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 function Square(props) {
     return (
-        <button className="square" onClick={props.onClick}>
+        <button id={props.id} className="square" onClick={props.onClick}>
             {props.value}
         </button>
     );
@@ -15,6 +15,7 @@ class Board extends React.Component {
     renderSquare(i) {
         return (
         <Square
+            id={'square'+i}
             value={this.props.squares[i]}
             onClick={() => this.props.onClick(i)}
         />
@@ -95,6 +96,7 @@ class Game extends React.Component {
 
     jumpTo(step, length) {
         cleanBoldLinks(length);
+        cleanRedSquares();
         this.setState({
             stepNumber: step,
             xIsNext: (step % 2) === 0,
@@ -160,6 +162,12 @@ function calculateWinner(squares) {
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            const squareA = document.getElementById('square' + a);
+            const squareB = document.getElementById('square' + b);
+            const squareC = document.getElementById('square' + c);
+            squareA.style.color = 'red';
+            squareB.style.color = 'red';
+            squareC.style.color = 'red';
             return squares[a];
         }
     }
@@ -190,6 +198,13 @@ function cleanBoldLinks(length) {
     for (let i = 0; i < length; i++) {
         const node = document.getElementById('link' + i);
         node.style.fontWeight = '';
+    }
+}
+
+function cleanRedSquares() {
+    for (let i = 0; i < 9; i++) {
+        const node = document.getElementById('square' + i);
+        node.style.color = 'black';
     }
 }
 
